48305117e282cce5d56a99a62971344d8a527a1c
Catalin Negrila <catalin_negrila@yahoo.com>
2023-05-18 17:19:58 -0700

    Add multi-file support.

--- README.md
+++ README.md
@@ -4,13 +4,14 @@
 
 Command line parameters:
 ```
-USAGE: simulator --input-file-path <input.modraw> --output-file-path <output.modraw> [--speed <multiplier>] [--verbose]
+USAGE: sim --input-file-path <input-file-path> --output-file-path <output-file-path> [--speed <multiplier>] [--verbose]
 
 OPTIONS:
-  -i, --input-file-path <input.modraw>
-                          Input .modraw file path.
-  -o, --output-file-path <output.modraw>
-                          Output .modraw file path.
+  -i, --input-file-path <input-file-path>
+                          Input .modraw file path, or @listOfFiles.txt, or
+                          folder to scan for .modraw files.
+  -o, --output-file-path <output-file-path>
+                          Output .modraw file path, or folder to write to.
   -s, --speed <multiplier>
                           Time multiplier. (default: 1.0)
   -v, --verbose           Show extra information.
@@ -22,10 +23,31 @@ Building and invoking:
 cd sim
 swift build -c release
 .build/release/sim -i original.modraw -o simulated.modraw -s 2 -v
+.build/release/sim -i ./one_folder/original.modraw -o ./another_folder -s 2 -v
+.build/release/sim -i ./folder_of_modraws -o ./output_folder -s 2 -v
+.build/release/sim -i @list_of_modraws.txt -o ./output_folder -s 2 -v
 ```
 
-One line way to build and run:
-```
-cd sim
-swift run -c release sim -i original.modraw -o simulated.modraw -s 2 -v
-```
+Input can be:
+- a single .modraw file, or
+- a folder that will be scanned for all .modraw files, or
+- a text file containing a list of .modraw file names or paths.
+
+If a text file is passed in, the path to the file must be prefixed by a "@" character.
+
+If the file names or paths contained in this text-based list file are relative (i.e. they
+don't start with "/"), they will be considered relative to the location where the text file is located,
+as opposed to the current folder or wherever the simulator resides. The order of the .modraw
+files in this list is respected. Lines beginning with '#' are considered comments and ignored.
+
+If a folder gets passed in as input, the .modraw files contained within will be processed
+in alphabetical order. To process them in any other order you will have to create a list
+file with the specific .modraw file paths you want and pass it in via the `-i @mylist.txt` syntax.
+
+Output can be a single .modraw file (which is permitted only if the input is a single
+.modraw file as well), or more flexibly it may be a folder. This folder must already exist so be sure
+to manually create it before invoking the simulator. Files in the output folder will be created
+with names mirroring the files in the input folder or text-based file list. If files with those same names
+already exist in the output folder they will be deleted, however any files with different names
+already present in the output folder will be preserved.
+ 
--- sim/src/main.swift
+++ sim/src/main.swift
@@ -8,56 +8,72 @@ func getCurrentTimeMs() -> Int {
     return Int(NSDate().timeIntervalSince1970 * 1000.0)
 }
 
-// If you prefer writing in a "script" style, you can call `parseOrExit()` to
-// parse a single `ParsableArguments` type from command-line arguments.
-let options = SimulatorOptions.parseOrExit()
-
-if (options.verbose) {
-    print("Reading from '\(options.inputFileUrl.path)'")
-    print("Writing to '\(options.outputFileUrl.path)'")
-    print("Running at \(options.speed)x speed")
-}
-
-let inputFileData = try Data(contentsOf: options.inputFileUrl)
-var inputFileParser = Parser(data: inputFileData)
-let outputFileUrl = options.outputFileUrl
-try deleteFile(fileURL: outputFileUrl)
-
-let header = inputFileParser.parseHeader()
-if header == nil {
-    throw MyError.runtimeError("Invalid file format. Could not parse header.")
-}
-try header!.appendToURL(fileURL: outputFileUrl)
-
-let som = inputFileParser.parsePacket()
-if som == nil {
-    throw MyError.runtimeError("Expected SOM first packet.")
-}
-try som!.data.appendToURL(fileURL: outputFileUrl)
-
-let startTime = getCurrentTimeMs()
+let options = SimOptions.parseOrExit()
+print("Running at \(options.speed)x speed")
 
 let dateFormatter = DateFormatter()
 dateFormatter.dateFormat = "yyyy/MM/dd HH:mm:ss.SS"
 dateFormatter.timeZone = TimeZone(abbreviation: "UTC")
 
-var packet = inputFileParser.parsePacket()
-while packet != nil {
-    if packet!.timeOffsetMs == nil || packet!.date == nil {
-        throw MyError.runtimeError("Expected timestamp on packet.")
-    }
-    if (options.verbose) {
-        let date = dateFormatter.string(for: packet!.date!)
-        print("\(inputFileParser.progress())% - T\(date!) \(packet!.signature)")
-    } else {
-        print(" Progress: \(inputFileParser.progress())% ", terminator: "\r")
-        fflush(stdout)
-    }
-    let relativeTime = getCurrentTimeMs() - startTime
-    if packet!.timeOffsetMs! > relativeTime {
-        usleep(UInt32(Double(packet!.timeOffsetMs! - relativeTime) / options.speed))
-    }
-    try packet!.data.appendToURL(fileURL: outputFileUrl)
-    packet = inputFileParser.parsePacket()
+let inputFileUrlList = try options.inputFileUrlList
+for inputFileUrl in inputFileUrlList {
+    let outputFileUrl = options.outputFileUrl(inputFileUrl)
+    try deleteFile(fileURL: outputFileUrl)
 }
+
+let inputFileUrlListWithIndex = zip(inputFileUrlList.indices, inputFileUrlList)
+for (index, inputFileUrl) in inputFileUrlListWithIndex {
+    let outputFileUrl = options.outputFileUrl(inputFileUrl)
+
+    print("\nFile \(index + 1) of \(inputFileUrlList.count):")
+    print(" Reading '\(inputFileUrl.path)'")
+    print(" Writing '\(outputFileUrl.path)'")
+
+    let inputFileData = try Data(contentsOf: inputFileUrl)
+    var inputFileParser = Parser(data: inputFileData)
+
+    let header = inputFileParser.parseHeader()
+    if header == nil {
+        throw MyError.runtimeError("Invalid file format. Could not parse header.")
+    }
+    try header!.appendToURL(fileURL: outputFileUrl)
+
+    let som = inputFileParser.parsePacket()
+    if som == nil {
+        throw MyError.runtimeError("Expected SOM first packet.")
+    }
+    try som!.data.appendToURL(fileURL: outputFileUrl)
+
+    let startTime = getCurrentTimeMs()
+    var lastProgress = -1.0
+
+    var packet = inputFileParser.parsePacket()
+    while packet != nil {
+        if packet!.timeOffsetMs == nil || packet!.date == nil {
+            throw MyError.runtimeError("Expected timestamp on packet.")
+        }
+
+        let currentProgress = inputFileParser.progress()
+        if lastProgress != currentProgress {
+            if options.verbose {
+                let date = dateFormatter.string(for: packet!.date!)
+                print("\(currentProgress)% - T\(date!) \(packet!.signature)")
+            } else {
+                print(" Progress: \(currentProgress)% ", terminator: "\r")
+                fflush(stdout)
+            }
+            lastProgress = currentProgress
+        }
+
+        let relativeTime = getCurrentTimeMs() - startTime
+        if packet!.timeOffsetMs! > relativeTime {
+            usleep(UInt32(Double(packet!.timeOffsetMs! - relativeTime) / options.speed))
+        }
+
+        try packet!.data.appendToURL(fileURL: outputFileUrl)
+        packet = inputFileParser.parsePacket()
+    }
+}
+
 print("Simulation completed.")
+
